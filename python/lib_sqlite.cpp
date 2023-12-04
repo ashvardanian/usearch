07ce48268a68132d74550d0b1e9d12bba88ec13e
Ash Vardanian <1983160+ashvardanian@users.noreply.github.com>
2023-12-04 13:52:47 -0800

    Add: Levenshtein distance support for SQLite3

--- python/lib_sqlite.cpp
+++ python/lib_sqlite.cpp
@@ -13,6 +13,8 @@
 #include <sqlite3ext.h>
 SQLITE_EXTENSION_INIT1
 
+static sz_string_view_t temporary_memory = {NULL, 0};
+
 template <scalar_kind_t scalar_kind_ak> struct parsed_scalar_kind_gt {
     using type = f32_t;
     static constexpr scalar_kind_t kind = scalar_kind_t::f32_k;
@@ -24,10 +26,11 @@ template <> struct parsed_scalar_kind_gt<scalar_kind_t::f64_k> {
 };
 
 template <scalar_kind_t scalar_kind_ak, metric_kind_t metric_kind_ak>
-static void sqlite_dense(sqlite3_context* context, int argc, sqlite3_value** argv) {
+static void sqlite_dense_export(sqlite3_context* context, int argc, sqlite3_value** argv,
+                                distance_punned_t* distance_out, char const** error_message_out) {
 
     if (argc < 2) {
-        sqlite3_result_error(context, "Distance function expects at least two arguments", -1);
+        *error_message_out = "Distance function expects at least two arguments";
         return;
     }
 
@@ -41,7 +44,7 @@ static void sqlite_dense(sqlite3_context* context, int argc, sqlite3_value** arg
         int bytes1 = sqlite3_value_bytes(argv[0]);
         int bytes2 = sqlite3_value_bytes(argv[1]);
         if (bytes1 != bytes2) {
-            sqlite3_result_error(context, "Vectors have different number of dimensions", -1);
+            *error_message_out = "Vectors have different number of dimensions";
             return;
         }
 
@@ -49,7 +52,7 @@ static void sqlite_dense(sqlite3_context* context, int argc, sqlite3_value** arg
         metric_t metric = metric_t(dimensions, metric_kind_ak, scalar_kind_ak);
         distance_punned_t distance =
             metric(reinterpret_cast<byte_t const*>(vec1), reinterpret_cast<byte_t const*>(vec2));
-        sqlite3_result_double(context, distance);
+        *distance_out = distance;
     }
 
     // Worst case is to have JSON arrays or comma-separated values
@@ -61,7 +64,7 @@ static void sqlite_dense(sqlite3_context* context, int argc, sqlite3_value** arg
         size_t commas1 = sz_count_char_swar(vec1, bytes1, ",");
         size_t commas2 = sz_count_char_swar(vec2, bytes2, ",");
         if (commas1 != commas2) {
-            sqlite3_result_error(context, "Vectors have different number of dimensions", -1);
+            *error_message_out = "Vectors have different number of dimensions";
             return;
         }
 
@@ -93,7 +96,7 @@ static void sqlite_dense(sqlite3_context* context, int argc, sqlite3_value** arg
             std::from_chars_result result1 = std::from_chars(vec1, vec1 + bytes1, parsed1[i]);
             std::from_chars_result result2 = std::from_chars(vec2, vec2 + bytes2, parsed2[i]);
             if (result1.ec != std::errc() || result2.ec != std::errc()) {
-                sqlite3_result_error(context, "Number can't be parsed", -1);
+                *error_message_out = "Number can't be parsed";
                 return;
             }
             bytes1 -= result1.ptr - vec1;
@@ -106,7 +109,7 @@ static void sqlite_dense(sqlite3_context* context, int argc, sqlite3_value** arg
             char* parsed2_end = vec2 + bytes2;
             parsed2[i] = std::strtod(vec2, &parsed2_end);
             if (vec1 == parsed1_end || vec2 == parsed2_end) {
-                sqlite3_result_error(context, "Number can't be parsed", -1);
+                *error_message_out = "Number can't be parsed";
                 return;
             }
             bytes1 -= parsed1_end - vec1;
@@ -126,7 +129,7 @@ static void sqlite_dense(sqlite3_context* context, int argc, sqlite3_value** arg
         metric_t metric = metric_t(dimensions, metric_kind_ak, parsed_scalar_kind_gt<scalar_kind_ak>::kind);
         distance_punned_t distance =
             metric(reinterpret_cast<byte_t const*>(parsed1), reinterpret_cast<byte_t const*>(parsed2));
-        sqlite3_result_double(context, distance);
+        *distance_out = distance;
     }
 
     // Less efficient, yet still common case is to have many scalar columns
@@ -141,17 +144,13 @@ static void sqlite_dense(sqlite3_context* context, int argc, sqlite3_value** arg
             case SQLITE_FLOAT: parsed1[i] = sqlite3_value_double(argv[i]); break;
             case SQLITE_INTEGER: parsed1[i] = sqlite3_value_int(argv[i]); break;
             case SQLITE_NULL: parsed1[i] = 0; break;
-            default:
-                sqlite3_result_error(context, "Scalar columns may only contain 32-bit integers, floats, or NULLs.", -1);
-                return;
+            default: *error_message_out = "Scalar columns may only contain 32-bit integers, floats, or NULLs."; return;
             }
             switch (sqlite3_value_type(argv[dimensions + i])) {
             case SQLITE_FLOAT: parsed2[i] = sqlite3_value_double(argv[dimensions + i]); break;
             case SQLITE_INTEGER: parsed2[i] = sqlite3_value_int(argv[dimensions + i]); break;
             case SQLITE_NULL: parsed2[i] = 0; break;
-            default:
-                sqlite3_result_error(context, "Scalar columns may only contain 32-bit integers, floats, or NULLs.", -1);
-                return;
+            default: *error_message_out = "Scalar columns may only contain 32-bit integers, floats, or NULLs."; return;
             }
         }
 
@@ -159,11 +158,65 @@ static void sqlite_dense(sqlite3_context* context, int argc, sqlite3_value** arg
         metric_t metric = metric_t(dimensions, metric_kind_ak, parsed_scalar_kind_gt<scalar_kind_ak>::kind);
         distance_punned_t distance =
             metric(reinterpret_cast<byte_t const*>(parsed1), reinterpret_cast<byte_t const*>(parsed2));
-        sqlite3_result_double(context, distance);
+        *distance_out = distance;
     }
     // Unsupported arguments combination
     else {
-        sqlite3_result_error(context, "Number of columns in two vectors must be divisible by two", -1);
+        *error_message_out = "Number of columns in two vectors must be divisible by two";
+    }
+}
+
+template <scalar_kind_t scalar_kind_ak, metric_kind_t metric_kind_ak>
+static void sqlite_dense(sqlite3_context* context, int argc, sqlite3_value** argv) {
+    distance_punned_t result = 0;
+    char const* error_message = NULL;
+    sqlite_dense_export<scalar_kind_ak, metric_kind_ak>(context, argc, argv, &result, &error_message);
+    if (error_message != NULL)
+        sqlite3_result_error(context, error_message, -1);
+    else
+        sqlite3_result_double(context, result);
+}
+
+static void sqlite_haversine_meters(sqlite3_context* context, int argc, sqlite3_value** argv) {
+    distance_punned_t result = 0;
+    char const* error_message = NULL;
+    sqlite_dense_export<scalar_kind_t::f64_k, metric_kind_t::haversine_k>(context, argc, argv, &result, &error_message);
+    if (error_message != NULL)
+        sqlite3_result_error(context, error_message, -1);
+    else
+        sqlite3_result_double(context, result * 6371009);
+}
+
+static void sqlite_levenshtein(sqlite3_context* context, int argc, sqlite3_value** argv) {
+    // This function always gets two arguments
+    int type1 = sqlite3_value_type(argv[0]);
+    int type2 = sqlite3_value_type(argv[1]);
+    if (type1 != SQLITE_TEXT || type2 != SQLITE_TEXT) {
+        sqlite3_result_error(context, "Levenshtein distance function expects two text arguments", -1);
+        return;
+    }
+
+    // Get the strings
+    char const* str1 = (char const*)sqlite3_value_text(argv[0]);
+    char const* str2 = (char const*)sqlite3_value_text(argv[1]);
+    size_t bytes1 = (size_t)sqlite3_value_bytes(argv[0]);
+    size_t bytes2 = (size_t)sqlite3_value_bytes(argv[1]);
+
+    size_t memory_needed = sz_levenshtein_memory_needed(bytes1, bytes2);
+    if (temporary_memory.length < memory_needed) {
+        temporary_memory.start = (char*)sqlite3_realloc64((void*)temporary_memory.start, memory_needed);
+        temporary_memory.length = memory_needed;
+    }
+
+    levenshtein_distance_t distance = sz_levenshtein(str1, bytes1, str2, bytes2, 255, (void*)temporary_memory.start);
+    sqlite3_result_int(context, distance);
+}
+
+static void cleanup_temporary_memory(void* userData) {
+    if (temporary_memory.start != NULL) {
+        sqlite3_free((void*)temporary_memory.start);
+        temporary_memory.start = NULL;
+        temporary_memory.length = 0;
     }
 }
 
@@ -176,14 +229,20 @@ extern "C" PYBIND11_MAYBE_UNUSED PYBIND11_EXPORT int sqlite3_compiled_init( //
     int flags = SQLITE_UTF8 | SQLITE_DETERMINISTIC | SQLITE_INNOCUOUS;
     int num_params = -1; // Any number will be accepted
 
+    sqlite3_create_function(db, "distance_haversine_meters", num_params, flags, NULL, sqlite_haversine_meters, NULL,
+                            NULL);
+    sqlite3_create_function_v2(db, "distance_levenshtein", 2, flags, NULL, sqlite_levenshtein, NULL, NULL,
+                               cleanup_temporary_memory);
+
     sqlite3_create_function(db, "distance_hamming_binary", num_params, flags, NULL,
                             sqlite_dense<scalar_kind_t::b1x8_k, metric_kind_t::hamming_k>, NULL, NULL);
     sqlite3_create_function(db, "distance_jaccard_binary", num_params, flags, NULL,
                             sqlite_dense<scalar_kind_t::b1x8_k, metric_kind_t::jaccard_k>, NULL, NULL);
-    sqlite3_create_function(db, "distance_haversine_meters", num_params, flags, NULL,
+
+    sqlite3_create_function(db, "distance_haversine_f32", num_params, flags, NULL,
+                            sqlite_dense<scalar_kind_t::f32_k, metric_kind_t::haversine_k>, NULL, NULL);
+    sqlite3_create_function(db, "distance_haversine_f64", num_params, flags, NULL,
                             sqlite_dense<scalar_kind_t::f64_k, metric_kind_t::haversine_k>, NULL, NULL);
-    // sqlite3_create_function(db, "distance_levenshtein", num_params, flags, NULL,
-    //                         sqlite_dense<scalar_kind_t::u8_k, metric_kind_t::haversine_k>, NULL, NULL);
 
     sqlite3_create_function(db, "distance_sqeuclidean_f64", num_params, flags, NULL,
                             sqlite_dense<scalar_kind_t::f64_k, metric_kind_t::l2sq_k>, NULL, NULL);
